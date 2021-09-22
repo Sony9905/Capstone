65224d7b7ae4ced7e20162b4c4af13a0cf6c97f8
Sony <1sony9905@gmail.com>
2021-09-22 14:04:57 -0400

    all files for frontend added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import Signin from './Components/Signin/Signin';
+import Register from './Components/Register/Register';
+import Card from './Components/Card/Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      route: 'signin',
+      isSignedIn: false
+    }
+  }
+
+
+   onRouteChange = (route) => {
+     if (route ==='signout') {
+       this.setState({isSignedIn : false})
+     }else if (route === 'home') {
+       this.setState({isSignedIn:true})
+     }
+    this.setState({route: route});
+   }
+   
+  render() {
+    return (
+      <div className="App">
+              <Particles className='particles'
+                params={{
+                  particles: {
+                    line_linked: {
+                      shadow: {
+                        enable: true,
+                        color: "#3CA9D1",
+                        blur: 5
+                      }
+                    }
+                  }
+                }}
+              />
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
+        {this.state.route === 'home' ?
+          <div>
+            <Logo/>
+            <Card/>
+          </div> : 
+          (
+            this.state.route ==='signin' ?
+             <Signin onRouteChange={this.onRouteChange}/>
+            :<Register onRouteChange={this.onRouteChange} />
+          )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
